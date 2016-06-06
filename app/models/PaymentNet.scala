552166c72f1806125adaa646759d52e0c05014b1
Ashrith Mekala <ashrith@me.com>
2016-06-05 19:29:23 -0700

    Add more payment net's

--- app/models/PaymentNet.scala
+++ app/models/PaymentNet.scala
@@ -8,7 +8,11 @@ object PaymentNet extends Enumeration {
 
   type Net = Value
   val NET_30 = Value("30")
+  val NET_35 = Value("35")
+  val NET_40 = Value("40")
   val NET_45 = Value("45")
+  val NET_50 = Value("50")
+  val NET_55 = Value("55")
   val NET_60 = Value("60")
 
   def isNetType(s: String) = values.exists(_.toString == s)
